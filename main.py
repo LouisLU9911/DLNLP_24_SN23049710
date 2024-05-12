ebc68d8cd69515f29382330fadf0b33af619a5ed
LouisLU9911 <louislu9911@gmail.com>
2024-05-12 23:38:31 +0100

    fix: use the correct format for main.py

--- main.py
+++ main.py
@@ -1,63 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Entrypoint of this assignment."""
+import A.logger
 
-import logging
-import os
-import sys
+# ======================================================================================================================
+# TODO: Data preprocessing
+# data_train, data_val, data_test = data_preprocessing(args...)
+# ======================================================================================================================
+# Task A
+# TODO: model A
+# model_A = A(args...)                 # Build model object.
+# acc_A_train = model_A.train(args...) # Train model based on the training set (you should fine-tune your model based on validation set.)
+# acc_A_test = model_A.test(args...)   # Test model based on the test set.
+# Clean up memory/GPU etc...             # Some code to free memory if necessary.
 
-from A.logger import set_log_level
-
-
-def setup_parse():
-    import argparse
-
-    description = "DLNLP Final Assignment"
-    parser = argparse.ArgumentParser(description=description)
-    parser.set_defaults(whether_output=True)
-    parser.add_argument(
-        "-d",
-        "--debug",
-        help="Print lots of debugging statements",
-        action="store_const",
-        dest="loglevel",
-        const=logging.DEBUG,
-        default=logging.WARNING,
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="Be verbose",
-        action="store_const",
-        dest="loglevel",
-        const=logging.INFO,
-    )
-
-    args, _ = parser.parse_known_args()
-    return args
-
-
-def print_info():
-    print("-------------------------------------")
-    print("|      AMLS II Assignment 23-24     |")
-    print("|         Name: Zhaoyan Lu          |")
-    print("|        Student No: 23049710       |")
-    print("-------------------------------------")
-
-
-def main():
-    try:
-        args = setup_parse()
-        set_log_level(args.loglevel)
-
-        if args.action == "info":
-            print_info()
-        else:
-            raise Exception(f"Unsupported action: {args.action}")
-    except Exception as e:
-        print(e)
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
+# ======================================================================================================================
+# TODO: Print out your results with following format:
+# print("TA:{},{};TB:{},{};".format(acc_A_train, acc_A_test, acc_B_train, acc_B_test))
